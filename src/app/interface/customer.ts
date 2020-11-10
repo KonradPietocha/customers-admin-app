0e265ae28405505184344e1d55be857c71a6d4fb
KonradPietocha <konrad.pietocha@gazeta.pl>
2020-11-10 19:00:08 +0100

    fixes

--- src/app/interface/customer.ts
+++ src/app/interface/customer.ts
@@ -1,18 +1,9 @@
 export interface Customer {
-    name: string,
-    surname: string,
-    dateOfBirth: string,
-    branch: string,
-    branchSubcategory: string,
-    phone: number,
-    eMail: string
+    name: string;
+    surname: string;
+    dateOfBirth: string;
+    branch: string;
+    branchSubcategory: string;
+    phone: number;
+    eMail: string;
 }
-
-// ● Imię (tekst)
-// ● Nazwisko (tekst)
-// ● Data urodzenia (data, min: 18 lat)
-// ● Branża (lista jednokrotnego wyboru: Finanse, Media, Podróże)
-// ● Podkategoria (lista jednokrotnego wyboru zależna od branży: Finanse -> Bank,
-// Ubezpieczalnia; Media -> TV, Radio; Podróże -> Krajowe, Zagraniczne)
-// ● Numer telefonu (9 cyfr, nie może rozpoczynać się od 0)
-// ● Adres e-mail (format e-mail)
